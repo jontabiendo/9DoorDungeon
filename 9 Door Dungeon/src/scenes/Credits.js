c59a44a716b11a1e6fa2c98851dc2363563419b3
JonTabiendo <jontabiendo@gmail.com>
2024-08-10 12:34:27 -0400

    Credits Scene added

---
+++ 9 Door Dungeon/src/scenes/Credits.js
@@ -0,0 +1,34 @@
+import { Scene } from 'phaser';
+
+export class Credits extends Scene
+{
+    constructor ()
+    {
+        super('Credits');
+    }
+
+    create ()
+    {
+        this.cameras.main.setBackgroundColor(0xff0000);
+
+        this.add.image(512, 384, 'background').setAlpha(0.5);
+
+        this.add.text(512, 300, 'Credits', {
+            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.add.text(512, 400, 'Jonathan Tabiendo', {
+            fontFamily: 'Arial Black', fontSize: 48, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.input.once('pointerdown', () => {
+
+            this.scene.start('MainMenu');
+
+        });
+    }
+}

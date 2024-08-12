b6abd29c343fe1f36452c0fef3135f274563021f
JonTabiendo <jontabiendo@gmail.com>
2024-08-11 21:02:59 -0400

    player model centered on collider and animations started

--- 9 Door Dungeon/src/scenes/Game.js
+++ 9 Door Dungeon/src/scenes/Game.js
@@ -6,10 +6,9 @@ export class Game extends Scene
     {
         super('Game');
         this.player,
-        this.cursors
+        this.cursors,
+        this.attack
     }
-
-    
 
     preload()
     {
@@ -18,25 +17,18 @@ export class Game extends Scene
         this.load.image('castleGround', 'assets/platforms/wall_3.png');
 
         // sprites
-        this.load.spritesheet('mainRight',
-            'assets/characterAssets/main/RunRight.png',
+        this.load.spritesheet('run',
+            'assets/characterAssets/main/Run.png',
             { 
                 frameWidth: 128, 
                 frameHeight: 128
-            }
-        );
-        this.load.spritesheet('mainLeft',
-            'assets/characterAssets/main/RunLeft.png',
-            { 
-                frameWidth: 128, 
-                frameHeight: 70
             }
         );
         this.load.spritesheet('mainIdle',
             'assets/characterAssets/main/Idle.png',
             {
                 frameWidth: 128,
-                frameHeight: 70
+                frameHeight: 128
             }
         )
         this.load.spritesheet('mainAttack', 
@@ -46,6 +38,13 @@ export class Game extends Scene
                 frameHeight: 128
             }
         );
+        this.load.spritesheet('jump',
+            'assets/characterAssets/main/Jump.png',
+            {
+                frameWidth: 128,
+                frameHeight: 128
+            }
+        )
     }
 
     create ()
@@ -64,29 +63,27 @@ export class Game extends Scene
         platforms.create(700, 400, 'castleGround');
         platforms.create(50, 250, 'castleGround');
         platforms.create(750, 220, 'castleGround');
+        
+        this.player = this.physics.add.sprite(100, 100, 'idle');
+        this.player.setSize(30, 55)
+        this.player.body.offset.x = 50;
+        this.player.body.offset.y = 70;
 
-        console.log(firstPlat.getBounds())
         
-        this.player = this.physics.add.sprite(100, 100, 'idle').setOrigin(0, 64);
-        
-        this.player.setBounce(0.2);
+        // this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
         this.player.body.setGravity(300)
 
-        this.physics.add.collider(this.player, 
-            platforms, 
-            null,
-            (player, platform) => 
-            {
-                return this.player.body.velocity.y >= 0;
-            }
-        );
+        this.physics.add.collider(this.player, platforms);
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
+        this.attack = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+        // console.log(Phaser.Input.Keyboard.Key)
+
         this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('mainLeft', { start: 7, end: 0 }),
+            key: 'run',
+            frames: this.anims.generateFrameNumbers('run', { start: 7, end: 0 }),
             frameRate: 10,
             repeat: -1
         });
@@ -99,14 +96,26 @@ export class Game extends Scene
         })
 
         this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('mainRight', { start: 0, end: 7 }),
-            frameRate: 10,
-            repeat: -1
-        });
+            key: 'jump',
+            frames: this.anims.generateFrameNumbers('jump', { start: 0, end: 1}),
+            frameRate: 10
+        })
 
         this.anims.create({
-            key: 'mainAttack',
+            key: 'land',
+            frames: this.anims.generateFrameNumbers('jump', { start: 6, end: 7}),
+            frameRate: 10
+        })
+
+        this.anims.create({
+            key: 'falling',
+            frames: this.anims.generateFrameNumbers('jump', { start: 3, end: 4}),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'attack',
             frames: this.anims.generateFrameNumbers('mainAttack', { start: 0, end: 5}),
             frameRate: 10,
             repeat: -1
@@ -122,17 +131,23 @@ export class Game extends Scene
     update()
     {
         // console.log(this.player)
+        if (this.player.body.velocity.x < 0) {
+            this.player.setFlipX(true)
+        } else {
+            this.player.setFlipX(false)
+        }
+
         if (this.cursors.left.isDown)
         {
             this.player.setVelocityX(-160);
 
-            this.player.anims.play('left', true);
+            this.player.anims.play('run', true);
 
         } else if (this.cursors.right.isDown)
         {
             this.player.setAccelerationX(160);
 
-            this.player.anims.play('right', true);
+            this.player.anims.play('run', true);
 
         } else
         {
@@ -142,14 +157,21 @@ export class Game extends Scene
 
         }
 
-        if (this.cursors.down.isDown) 
-            {
-                this.player.anims.play('mainAttack', true)
-            } 
-
         if (this.cursors.up.isDown && this.player.body.touching.down)
         {
             this.player.setVelocityY(-330);
+
+            this.player.anims.play('jump')
+        }
+
+        if (!this.player.body.touching.down)
+        {
+            this.player.anims.play('falling')
+        }
+
+        // console.log(this.events)
+        if (this.attack.isDown) {
+            this.player.anims.play('attack')
         }
     }
 }
